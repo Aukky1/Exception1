6f8e0da8ea3c33de21917023e47a663b49e23cfa
Thitiphat <thitiphat.tana@kmutt.ac.th>
2023-03-29 12:26:27 +0700

    Add projects

---
+++ src/exception1/Exception1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exception1;
+
+import java.util.Scanner;
+
+public class Exception1 {
+
+   
+    public static void main(String[] args) {
+           throw new ArithmeticException("Divisor cannot be zero");
+    }
+    
+}
